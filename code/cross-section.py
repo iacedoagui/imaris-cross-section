d97473a60e2d89b8b5b3e1e443a73129485bde03
Ivan Acedo <iacedoagui@miners.utep.edu>
2026-04-21 13:20:59 -0600

    Writing code just to retrieve images and stress test to see how I will continue creating/forming the arrays so it doesnt take so much memory.

--- code/cross-section.py
+++ code/cross-section.py
@@ -1,3 +1,6 @@
+# Code for Debugging, stress test and just trying things. Not serious code.
+# Ivan Acedo Aguilar
+
 from datetime import datetime, timedelta
 from tifffile import tifffile
 import numpy as np
@@ -8,7 +11,7 @@ start = datetime.now()
 print(f"Started at: {start.strftime('%H:%M:%S')}\n")
 
 #Load file
-path = '/Volumes/Extreme SSD/Ivan/NeuN Af647 3.6x 22_029.ims'
+path = "/mnt/e/IMS NeuN For Ivan - MQ/GLP1_NeuN_9x_missing_DONE.ims"
 print(f"Loading IMS file: {path}\n")
 
 data = ims(path, ResolutionLevelLock=0)
@@ -25,7 +28,16 @@ print(f"Data chunks: {data.chunks}")
 print(f"Data dimensions: {data.ndim}\n")
 
 
-volume = data[0, 0, 0:(data.shape[2]-1):2, 0:(data.shape[3]-1):2, 0:(data.shape[4]-1):2]
+#volume = data[0, 0, 0:(data.shape[2]-1):2, 0:(data.shape[3]-1):2, 0:(data.shape[4]-1):2]
+
+
+volume = np.zeros((3,(data.shape[2]-1)//3,(data.shape[3]-1)//3,(data.shape[4]-1)//3), np.uint16)
+
+volume[0] = data[0, 0, 0:((data.shape[2]-1)//3):1, 0:((data.shape[3]-1)//3):1, 0:((data.shape[4]-1)//3):1]
+volume[1] = data[0, 0, ((data.shape[2]-1)//3):(((data.shape[2]-1)//3)*2):1, ((data.shape[3]-1)//3):(((data.shape[3]-1)//3)*2):1, ((data.shape[4]-1)//3):(((data.shape[4]-1)//3)*2):1]
+volume[2] = data[0, 0, (((data.shape[2]-1)//3)*2):(((data.shape[2]-1)//3)*3):1, (((data.shape[3]-1)//3)*2):(((data.shape[3]-1)//3)*3):1, (((data.shape[4]-1)//3)*2):(((data.shape[4]-1)//3)*3):1]
+
+
 print(f"\nVolume shape: {volume.shape}")  # Should print (Z, Y, X)
 
 
@@ -36,12 +48,14 @@ z_mid = volume.shape[0] // 2
 y_mid = volume.shape[1] // 2
 x_mid = volume.shape[2] // 2
 
+y_mid = [50, 60]
+
 # XY plane (Z-slice)
-slice_xy = volume[z_mid, :, :]
+slice_xy = volume[1, z_mid, :, :]
 # XZ plane (Y-slice)
-slice_xz = volume[:, y_mid, :]
+slice_xz = volume[1, :, y_mid, :]
 # YZ plane (X-slice)
-slice_yz = volume[:, :, x_mid]
+slice_yz = volume[1, :, :, x_mid]
 
 # 4. Save slices as images
 def save_slice(array, filename):
@@ -68,9 +82,12 @@ def save_slice(array, filename):
 
     print(f"Saved: {filename}")
 
-save_slice(slice_xy, "cross_section_xy_Res_0.tiff")
-save_slice(slice_xz, "cross_section_xz_Res_0.tiff")
-save_slice(slice_yz, "cross_section_yz_Res_0.tiff")
+save_slice(slice_xy, "cross_section_xy_Res_0_Num_2.tiff")
+save_slice(slice_xz, "cross_section_xz_Res_0_Num_2.tiff")
+save_slice(slice_yz, "cross_section_yz_Res_0_Num_2.tiff")
+
+print(f"Data is a Numpy array = {isinstance(data, np.ndarray)}")
+print(f"Volume is a Numpy array = {isinstance(volume, np.ndarray)}")
 
 end = datetime.now()
 print(f"\nFinished at: {end.strftime('%H:%M:%S')}")
