06ee3a640922fe37a689980141ae14e5c3274049
Ivan Acedo <iacedoagui@miners.utep.edu>
2026-04-27 13:37:58 -0600

    Testing is working nicely

--- code/cross-section-testing.py
+++ code/cross-section-testing.py
@@ -9,6 +9,8 @@ from imaris_ims_file_reader import ims
 
 # 4. Save slices as images
 def save_slice(array, filename):
+    
+
     # Convert to float for safe normalization
     slice_float = array.astype(np.float32)
 
@@ -32,8 +34,9 @@ def save_slice(array, filename):
 
     print(f"Saved: {filename}")
 
-count = 3
+count = 5
 res = 0
+n_slices = 70
 
 start = datetime.now()
 print(f"Started at: {start.strftime('%H:%M:%S')}\n")
@@ -64,107 +67,28 @@ y = ims_data.shape[3]
 x = ims_data.shape[4]
 
 # Split data into 5 different numpy arrays
-z1 = z // 20
-z2 = 2 * z // 20
-z3 = 3 * z // 20
-z4 = 4 * z // 20
-z5 = 5 * z // 20
-z6 = 6 * z // 20
-z7 = 7 * z // 20
-z8 = 8 * z // 20
-z9 = 9 * z // 20
-z10 = 10 * z // 20
-z11 = 11 * z // 20
-z12 = 12 * z // 20
-z13 = 13 * z // 20
-z14 = 14 * z // 20
-z15 = 15 * z // 20
-z16 = 16 * z // 20
-z17 = 17 * z // 20
-z18 = 18 * z // 20
-z19 = 19 * z // 20
+
+z_splits = [i * z // n_slices for i in range(n_slices + 1)]
+
 y1, y2, y3, y4 = y//5, 2*y//5, 3*y//5, 4*y//5
 x1, x2, x3, x4 = x//5, 2*x//5, 3*x//5, 4*x//5
 
 # ims_data[Time?, Channel, Z, Y, X]
 for i in range(20):
-    volume = [None] * 20
+    start_t = datetime.now()
+    print(f"\n------->Slice started at: {start_t.strftime('%H:%M:%S')}")
 
-    v0 = None
-    v1 = None
-    v2 = None
-    v3 = None
-    v4 = None
-    v5 = None
-    v6 = None
-    v7 = None
-    v8 = None
-    v9 = None
-    v10 = None
-    v11 = None
-    v12 = None
-    v13 = None
-    v14 = None
-    v15 = None
-    v16 = None
-    v17 = None
-    v18 = None
-    v19 = None
+    slice_vol = ims_data[0, :, z_splits[i]:z_splits[i+1], :, :]
 
-    if i == 0:
-        v0 = ims_data[0, :, 0:z1, :, :]
-    elif i == 1:
-        v1 = ims_data[0, :, z1:z2, :, :]
-    elif i == 2:
-        v2 = ims_data[0, :, z2:z3, :, :]
-    elif i == 3:
-        v3 = ims_data[0, :, z3:z4, :, :]
-    elif i == 4:
-        v4 = ims_data[0, :, z4:z5, :, :]
-    elif i == 5:
-        v5 = ims_data[0, :, z5:z6, :, :]
-    elif i == 6:
-        v6 = ims_data[0, :, z6:z7, :, :]
-    elif i == 7:
-        v7 = ims_data[0, :, z7:z8, :, :]
-    elif i == 8:
-        v8 = ims_data[0, :, z8:z9, :, :]
-    elif i == 9:
-        v9 = ims_data[0, :, z9:z, :, :]
-    elif i == 10:
-        v10 = ims_data[0, :, 0:z1, :, :]
-    elif i == 11:
-        v11 = ims_data[0, :, z1:z2, :, :]
-    elif i == 12:
-        v12 = ims_data[0, :, z2:z3, :, :]
-    elif i == 13:
-        v13 = ims_data[0, :, z3:z4, :, :]
-    elif i == 14:
-        v14 = ims_data[0, :, z4:z5, :, :]
-    elif i == 15:
-        v15 = ims_data[0, :, z5:z6, :, :]
-    elif i == 16:
-        v16 = ims_data[0, :, z6:z7, :, :]
-    elif i == 17:
-        v17 = ims_data[0, :, z7:z8, :, :]
-    elif i == 18:
-        v18 = ims_data[0, :, z8:z9, :, :]
-    elif i == 19:
-        v19 = ims_data[0, :, z9:z, :, :]
-
-    volume = [v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20]
-
-
-
-    print(f"\nVolume shape: {volume[i].shape}")  # Should print (c, Z, Y, X)
+    print(f"\nVolume shape: {slice_vol.shape}")  # Should print (c, Z, Y, X)
 
     # 3. Extract middle cross sections (Orthoslices)
-    z_mid = volume[i].shape[1] // 2
-    y_mid = volume[i].shape[2] // 2
-    x_mid = volume[i].shape[3] // 2
+    z_mid = slice_vol.shape[1] // 2
+    y_mid = slice_vol.shape[2] // 2
+    x_mid = slice_vol.shape[3] // 2
 
     # XY plane (Z-slice)
-    slice_xy = volume[i][:, z_mid, :, :]
+    slice_xy = slice_vol[:, z_mid, :, :]
     # XZ plane (Y-slice)
     #slice_xz = volume[i][:, :, y_mid, :]
     # YZ plane (X-slice)
@@ -173,6 +97,12 @@ for i in range(20):
     save_slice(slice_xy, f"24_014_Lectin488+NeuN647_{i}_xy_{count}.tiff")
     #save_slice(slice_xz, f"24_014_Lectin488+NeuN647_{i}_xz_{count}.tiff")
     #save_slice(slice_yz, f"24_014_Lectin488+NeuN647_{i}_yz_{count}.tiff")
+
+    del slice_vol
+
+    end_t = datetime.now()
+    print(f"Slice finished at: {end_t.strftime('%H:%M:%S')}")
+    print(f"Total slice time: {end_t - start_t}<-------\n")
 
 end = datetime.now()
 print(f"\nFinished at: {end.strftime('%H:%M:%S')}")
